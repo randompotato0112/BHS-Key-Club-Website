d2df27e5923bcd8f2ed6b5cf71dea34ecba6ad32
Hemish Duri <durihemish@gmail.com>
2024-07-22 08:49:56 -0500

    added secret button

--- src/pages/My Account.b6ros.js
+++ src/pages/My Account.b6ros.js
@@ -3,8 +3,9 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
-
+    $w('#button1').onClick( () => {
+        $w('#text3').text = 'Hello from Velo!';
+    } );
     // To select an element by ID use: $w('#elementID')
-
     // Click 'Preview' to run your code
 });
